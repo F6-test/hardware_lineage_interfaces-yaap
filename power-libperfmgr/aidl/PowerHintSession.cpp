95b6c5d17e6aba9b0a35666f0b1a9f431868d2c2
Pablo Ceballos <pceballos@google.com>
2025-08-01 12:24:26 -0700

    ADPF: Exclude SystemUI from getting the higher initial ADPF boost
    SystemUI creates many short-lived ADPF sessions and they were all
    getting the higher initial boost for much of their lifetime. Exclude
    SystemUI (and nexuslauncher) from getting this additional extra boost.
    
    Bug: 395187033
    Test: atest libadpf_test
    Test: Take a trace and confirm the extra boost isn't active for sysui
    
    Change-Id: I8562065d9889f3e183450864fd8eadfa3f13211e

--- power-libperfmgr/aidl/PowerHintSession.cpp
+++ power-libperfmgr/aidl/PowerHintSession.cpp
@@ -191,7 +191,9 @@ PowerHintSession<HintManagerT, PowerSessionManagerT>::PowerHintSession(
                               ? std::make_unique<SessionRecords>(
                                         getAdpfProfile()->mMaxRecordsNum.value(),
                                         getAdpfProfile()->mJankCheckTimeFactor.value())
-                              : nullptr) {
+                              : nullptr),
+      mEnableHigherInitialHeuristicBoost(powerhal::flags::initial_hboost_severe() &&
+                                         mProcTag != ProcessTag::SYSTEM_UI) {
     ATRACE_CALL();
     ATRACE_INT(mAppDescriptorTrace->trace_target.c_str(), mDescriptor->targetNs.count());
     ATRACE_INT(mAppDescriptorTrace->trace_active.c_str(), mDescriptor->is_active.load());
@@ -213,8 +215,8 @@ PowerHintSession<HintManagerT, PowerSessionManagerT>::PowerHintSession(
 
     mPSManager->voteSet(mSessionId, AdpfVoteType::CPU_VOTE_DEFAULT, adpfConfig->mUclampMinInit,
                         kUclampMax, std::chrono::steady_clock::now(), mDescriptor->targetNs);
-    mJankyLevel = powerhal::flags::initial_hboost_severe() ? SessionJankyLevel::SEVERE
-                                                           : SessionJankyLevel::LIGHT;
+    mJankyLevel = mEnableHigherInitialHeuristicBoost ? SessionJankyLevel::SEVERE
+                                                     : SessionJankyLevel::LIGHT;
     ALOGV("PowerHintSession created: %s", mDescriptor->toString().c_str());
 }
 
@@ -362,12 +364,13 @@ ndk::ScopedAStatus PowerHintSession<HintManagerT, PowerSessionManagerT>::updateT
 template <class HintManagerT, class PowerSessionManagerT>
 void PowerHintSession<HintManagerT, PowerSessionManagerT>::resetSessionHeuristicStates() {
     mSessionRecords->resetRecords();
-    bool enableInitialSevere = powerhal::flags::initial_hboost_severe();
-    mJankyLevel = enableInitialSevere ? SessionJankyLevel::SEVERE : SessionJankyLevel::LIGHT;
+    mJankyLevel = mEnableHigherInitialHeuristicBoost ? SessionJankyLevel::SEVERE
+                                                     : SessionJankyLevel::LIGHT;
     mJankyFrameNum = 0;
     ATRACE_INT(mAppDescriptorTrace->trace_hboost_janky_level.c_str(),
                static_cast<int32_t>(mJankyLevel));
-    ATRACE_INT(mAppDescriptorTrace->trace_initial_severe_active.c_str(), enableInitialSevere);
+    ATRACE_INT(mAppDescriptorTrace->trace_initial_severe_active.c_str(),
+               mEnableHigherInitialHeuristicBoost);
     ATRACE_INT(mAppDescriptorTrace->trace_missed_cycles.c_str(), mJankyFrameNum);
     ATRACE_INT(mAppDescriptorTrace->trace_avg_duration.c_str(), 0);
     ATRACE_INT(mAppDescriptorTrace->trace_max_duration.c_str(), 0);
@@ -382,7 +385,7 @@ SessionJankyLevel PowerHintSession<HintManagerT, PowerSessionManagerT>::updateSe
     if (isLowFPS) {
         return SessionJankyLevel::LIGHT;
     }
-    if (powerhal::flags::initial_hboost_severe() && !areAllRecordsInitialized) {
+    if (mEnableHigherInitialHeuristicBoost && !areAllRecordsInitialized) {
         return SessionJankyLevel::SEVERE;
     }
 
@@ -426,7 +429,7 @@ void PowerHintSession<HintManagerT, PowerSessionManagerT>::updateHeuristicBoost(
     ATRACE_INT(mAppDescriptorTrace->trace_hboost_janky_level.c_str(),
                static_cast<int32_t>(mJankyLevel));
     ATRACE_INT(mAppDescriptorTrace->trace_initial_severe_active.c_str(),
-               powerhal::flags::initial_hboost_severe() && !areAllRecordsInitialized);
+               mEnableHigherInitialHeuristicBoost && !areAllRecordsInitialized);
     ATRACE_INT(mAppDescriptorTrace->trace_missed_cycles.c_str(), mJankyFrameNum);
     ATRACE_INT(mAppDescriptorTrace->trace_avg_duration.c_str(), avgDurationUs.value());
     ATRACE_INT(mAppDescriptorTrace->trace_max_duration.c_str(), maxDurationUs.value());
