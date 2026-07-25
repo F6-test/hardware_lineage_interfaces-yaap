b29331a8f4616fe9a4c2e5333caa41dde3508afd
Michael Bestas <mkbestas@lineageos.org>
2026-07-25 11:32:12 +0300

    power-libperfmgr: Remove pixelstats dependency
    Change-Id: I595b352293979dbd63090cc1933e4ec678a6f242

--- power-libperfmgr/aidl/service.cpp
+++ power-libperfmgr/aidl/service.cpp
@@ -25,13 +25,11 @@
 
 #include <thread>
 
-#include "MetricUploader.h"
 #include "Power.h"
 #include "PowerExt.h"
 #include "PowerSessionManager.h"
 #include "utils/ThermalStateListener.h"
 
-using aidl::google::hardware::power::impl::pixel::MetricUploader;
 using aidl::google::hardware::power::impl::pixel::Power;
 using aidl::google::hardware::power::impl::pixel::PowerExt;
 using aidl::google::hardware::power::impl::pixel::ThermalStateListener;
@@ -72,7 +70,6 @@ int main() {
     std::thread initThread([&]() {
         ::android::base::WaitForProperty(kPowerHalInitProp.data(), "1");
         HintManager::GetInstance()->Start();
-        MetricUploader::getInstance()->init();
         ThermalStateListener::getInstance()->init();
     });
     initThread.detach();
